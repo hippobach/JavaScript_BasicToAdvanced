6f40530791a9cb6ceedbf3bdce8945117d5c1ace
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2024-03-26 23:32:55 +0700

    Variable declaration and initialization

---
+++ Lesson1/main.js
@@ -0,0 +1,18 @@
+alert('Hello world!');
+alert('I am Xuan Bach');
+// Hàm hiển thị hộp nhập liệu
+prompt('Mời nhập tên của bạn:');
+
+// #JS-1
+// Khai báo biến
+var name;
+let age;
+
+// Khởi tạo biến
+var diemToan = 10;
+const doSoi = 100;
+let firstName;
+firstName = 'Alicensed';
+
+// Xuất giá trị của biến
+console.log(diemToan);
